58349987787bd63a490181f3dc4fed1282430da6
xavidop <4416096+xavidop@users.noreply.github.com>
2025-02-12 11:08:49 +0000

    Update Genkit formula to 1.0.4

--- Formula/genkit.rb
+++ Formula/genkit.rb
@@ -14,14 +14,6 @@ class Genkit < Formula
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
-  def uninstall
-    # Remove symlinks
-    rm Dir["#{bin}/genkit"]
-    
-    # Remove the package directory
-    rm_rf libexec
-  end
-
   test do
     assert_match "genkit", shell_output("#{bin}/genkit --help")
   end
