56feea832ba3926bf17cf0557e8a1bbfd31948a4
xavidop <4416096+xavidop@users.noreply.github.com>
2026-05-22 00:19:51 +0000

    Update Genkit formula to 1.35.0

--- Formula/genkit.rb
+++ Formula/genkit.rb
@@ -3,9 +3,9 @@ require "language/node"
 class Genkit < Formula
   desc "Firebase Genkit CLI"
   homepage "https://github.com/firebase/genkit"
-  url "https://registry.npmjs.org/genkit-cli/-/genkit-cli-1.34.0.tgz"
-  sha256 "8f17d5f46d2bb4a94452883944761dcf41be6f2ce4d2ffe340033b8bde30f431"
-  version "1.34.0"
+  url "https://registry.npmjs.org/genkit-cli/-/genkit-cli-1.35.0.tgz"
+  sha256 "79843d26de487594d22d9eba90b1bb985b4d455e2841f00fd88d42d6ce96d615"
+  version "1.35.0"
 
   depends_on "node"
 
